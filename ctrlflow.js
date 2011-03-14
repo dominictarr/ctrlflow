1476c18cc6d6d799b50949462aedf5968dc063b9
Dominic Tarr <dominic.tarr@gmail.com>
2011-03-14 17:20:57 +1300

    add width function to ctrlflow

--- ctrlflow.js
+++ ctrlflow.js
@@ -61,3 +61,45 @@ exports.defer = function (obj,commands){
     //execute commands, and then hook to the regular methods.
   }
 }
+
+exports.width = Queue
+
+function Queue(jobs,width){
+  if(!(this instanceof Queue)) return new Queue(jobs,width)
+
+  var self = this
+  
+  this.keys = Object.keys(jobs)
+  this.running = 0
+  this.jobs = jobs
+  this.width = width || 1
+  this.done = function (){}
+  
+  this.start = function (){
+    var k = self.keys.shift()
+    self.running ++
+    self.func(self.jobs[k],k,self.jobs)
+  }
+  
+  this.next = function (){
+    self.running --
+    if(self.keys.length){
+      self.start()
+    } else if (self.running == 0){
+        self.done()
+    }
+  }
+}
+
+Queue.prototype = {
+
+  forEach: 
+    function (func,done){
+      this.func = func
+      this.done = done || this.done 
+      var i = 0
+      while(i++ < this.width && this.keys.length)
+        this.start()
+    }
+}
+
