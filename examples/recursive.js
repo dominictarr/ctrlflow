7a88615ff7759b9a800e08faac1746d3c38b97e2
Dominic Tarr <dominic.tarr@gmail.com>
2011-09-06 00:55:48 +1000

    fix dangerous bug in an example, it was ronaming the cwd

--- examples/recursive.js
+++ examples/recursive.js
@@ -1,43 +1,34 @@
+
+//
+// switch the names of two files.
+//
+
 var ctrl    = require('../')
   , path    = require('path')
   , fs      = require('fs')
-  , file1   = path.resolve(process.argv[2])
-  , file2   = path.resolve(process.argv[3])
+  , d       = require('d-utils')
+  , rec = {}
+  
 
-function switchFiles (f1, f2, callback) {
-  var tmp = f1 + '_tmp'
+function rec (dir, callback) {
   ctrl([
-    [fs.rename, f1  , tmp ],
-    [fs.rename, f2  , f1  ],
-    [fs.rename, tmp , f2  ],
+    [fs.lstat, dir], //check is file
+    function (stat, callback) {
+      callback(!stat.isDirectory)
+    },
+    [fs.readdir, dir],
+    function (list, callback) {
+      var ls = 
+        d.mapKeys(list, function (key) {
+          return [[d.tryCatchPass(rec, function (err, callback) {
+            if(err === true)
+              callback()
+            else callback(err)    
+          }, d.fallthrough), path.join(dir, key)]]
+        })
+      console.log(ls)      
+      ctrl([ls])(callback)
+    }
   ])(callback)
 }
-
-//
-// error here, cannot call a seq in parallel yet!
-//
-
-var cat100 = 
-//  function (file,cb) {
-  ctrl([
-    fs.readFile,
-    ctrl.toAsync(function (buffer) {
-      return buffer.toString().slice(0,100)
-    })
-  ])//(file,cb)
-//}
-
-switchFiles(file1, file2, function (err) {
-
-if(err) throw err
-
-  ctrl([{
-    file1: [[cat100, file1]],
-    file2: [[cat100, file2]],
-    }
-  ])(function (err, data) {
-    if(err) throw err
-    console.log(data)
-  })
-
-})
+rec(process.cwd(), console.log)
---
+++ examples/switchNames.js
@@ -0,0 +1,48 @@
+
+//
+// switch the names of two files.
+//
+
+var ctrl    = require('../')
+  , path    = require('path')
+  , fs      = require('fs')
+  , file1   = process.argv[2] && path.resolve(process.argv[2])
+  , file2   = process.argv[3] && path.resolve(process.argv[3])
+
+//switch filenames of two files.
+
+function switchFiles (f1, f2, callback) {
+  var tmp = f1 + '_tmp'
+  ctrl([
+    [fs.rename, f1  , tmp ],
+    [fs.rename, f2  , f1  ],
+    [fs.rename, tmp , f2  ],
+  ])(callback)
+}
+
+//log the first 100 characters of a file
+
+var cat100 = 
+  ctrl([
+    fs.readFile,
+    ctrl.toAsync(function (buffer) {
+      return buffer.toString().slice(0,100)
+    })
+  ])
+
+//switch the names of two files, then log their first 100 chars, al get an directory listing too.
+if(!file1 || !file2)
+  throw new Error('try `cd ctrlflow/examples && node switchNames.js hello.txt bye.txt`')
+
+ctrl([
+  [switchFiles, file1, file2],
+  {
+    file1: [[cat100, file1]],
+    file2: [[cat100, file2]],
+    ls: [[fs.readdir, __dirname]]
+  }
+  ])
+(function (err, data) {
+  if(err) throw err
+  console.log(data)
+})
