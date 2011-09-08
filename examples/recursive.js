5c34d7106a9a4aecf5ca937dbd55c46c0f8bfa05
Dominic Tarr <dominic.tarr@gmail.com>
2011-09-08 10:17:12 +1000

    bump version 4.1

--- examples/recursive.js
+++ examples/recursive.js
@@ -6,29 +6,63 @@
 var ctrl    = require('../')
   , path    = require('path')
   , fs      = require('fs')
-  , d       = require('d-utils')
-  , rec = {}
-  
+  , d       = require('d-utils')  
 
-function rec (dir, callback) {
-  ctrl([
-    [fs.lstat, dir], //check is file
-    function (stat, callback) {
-      callback(!stat.isDirectory)
-    },
-    [fs.readdir, dir],
-    function (list, callback) {
-      var ls = 
-        d.mapKeys(list, function (key) {
-          return [[d.tryCatchPass(rec, function (err, callback) {
-            if(err === true)
-              callback()
-            else callback(err)    
-          }, d.fallthrough), path.join(dir, key)]]
+    var ls = 
+    ctrl([
+      function (fn, cb) {
+        fs.lstat(fn, function (err, stat) {
+          if(err)
+            cb(err)
+          else {
+            stat.name = fn
+            cb(null, stat)
+          }
         })
-      console.log(ls)      
-      ctrl([ls])(callback)
-    }
-  ])(callback)
-}
-rec(process.cwd(), console.log)
+      },
+      function tryParse (file, cb) {
+        if(/package\.json$/.exec(file.name)) {
+          try {
+            JSON.parse(fs.readFileSync(file.name))
+          } catch (err) {
+            console.log(file.name, err.message)
+          }
+          /*ctrl([
+            [fs.readFile, file.name, 'utf-8']
+          , ctrl.toAsync(JSON.parse)
+          , [d.fallthrough, file]
+          ])(cb)*/
+        }
+        //else
+          cb(null, file)
+      }
+    , function (file, next) {
+        if(file.isDirectory())
+          ctrl([
+            [fs.readdir, file.name]
+          , function (fn,next) {
+              next(null, d.map(d.filter(fn, /^[^.]/), function (v) {return path.join(file.name, v)}))
+            }
+          , ctrl.parallel.map(ls)
+          ])(next)
+        else
+          next(null, file)
+      }
+    ])
+
+ls(process.argv[2] || process.cwd(), function (err, data) {
+  if(err)
+    throw err 
+/*  function print(file) {
+    if(Array.isArray(file))
+      d.each(file, function (f) {
+        print(f)
+      })
+    else
+      console.log(file.name)
+  }
+  print(data)*/
+})
+
+  
+//ls(process.cwd(), console.log)
